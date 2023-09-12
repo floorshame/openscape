bf7fa24e27bcc64f8f791c8fbc0380acd47dbbd5
https404 <hippostars@gmail.com>
2023-09-12 11:29:38 +1000

    base

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+function update(_update) {
+    if (_update === "all") {
+        document.title = `openscape ~ ${game["version"]}`
+    }
+}
+update("all") // keep at bottom
---
+++ assets/js/variables.js
@@ -0,0 +1,13 @@
+save = { // saved
+
+}
+
+game = { // not saved
+    "version": "0.1.0.0",
+
+    "sidebar_tabs": {
+        "inventory": {
+            
+        }
+    }
+}
